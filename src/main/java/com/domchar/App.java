149a6487c75d4a33e45b0d5477cd3fd7b68144f4
Dominic Charcap <dcharcap@gmail.com>
2026-03-01 23:15:41 -0500

    Update version message

--- src/main/java/com/domchar/App.java
+++ src/main/java/com/domchar/App.java
@@ -2,7 +2,7 @@ package com.domchar;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Pipeline Demo Running! - version 2");
+        System.out.println("Pipeline Demo Running! - version 3");
         System.out.println("2 + 3 = " + add(2, 3));
     }
 
